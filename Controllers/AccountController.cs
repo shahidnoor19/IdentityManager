02ebfbf6d7ef24b40c710e062318f11211ed9819
shahidnoor19 <shahid19togo@gmail.com>
2023-05-21 01:11:10 +0500

    add more changes

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityManager.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityManager.Controllers
 {
     public class AccountController : Controller
     {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+
+        }
+
+
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            
+            return View(registerViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser{ UserName = model.Email, Email = model.Email, Name = model.Name };
+
+                var reuslt = await _userManager.CreateAsync(user,model.Password);
+
+                if (reuslt.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(reuslt);
+            }
+
+
+            return View(model);
+        }
+
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
