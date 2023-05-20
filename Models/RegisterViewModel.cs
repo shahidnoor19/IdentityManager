eafb9f8f866770b814868dc0ade93c920b6a148d
shahidnoor19 <shahid19togo@gmail.com>
2023-05-20 23:13:31 +0500

    First Commit

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityManager.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+
+
+
+        [Required]
+        [StringLength(100,ErrorMessage = "The {0} must be at leate {2} character long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+
+
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The Password and Confirmation Password do not Match")]
+        public string ConfirmPassword { get; set; }
+
+
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
